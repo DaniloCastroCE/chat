1446043b81b405d6f679c39438cb74091aaf1fae
DaniloCastroCE <38533079+DaniloCastroCE@users.noreply.github.com>
2025-04-11 21:30:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const path = require('path')
 const http = require('http')
 const server = http.createServer(app)
 const router = require('./src/router/router')
-const RedisStore = require('connect-redis')(session);
-const redis = require('redis');
 const connectServer = require('./src/server/Servidor')
 const session = require('express-session')
+const RedisStore = require('connect-redis')(session);
+const redis = require('redis');
 
 const redisClient = redis.createClient({
   host: 'localhost', // Ou a URL do seu servidor Redis
